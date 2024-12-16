1353469977bfcac5db9157a3f109a66e9bc23b9d
Mayank Webichi <mayankwebichi@gmail.com>
2024-12-17 00:09:40 +0530

    v4.0

--- xmlrpc0db0.php
+++ xmlrpc0db0.php
@@ -2,13 +2,13 @@
 	<service>
 		<engineName>WordPress</engineName>
 		<engineLink>https://wordpress.org/</engineLink>
-		<homePageLink>https://thewedders.in/</homePageLink>
+		<homePageLink>https://rawleavesphotography.in/</homePageLink>
 		<apis>
-			<api name="WordPress" blogID="1" preferred="true" apiLink="https://thewedders.in/xmlrpc.php" />
-			<api name="Movable Type" blogID="1" preferred="false" apiLink="https://thewedders.in/xmlrpc.php" />
-			<api name="MetaWeblog" blogID="1" preferred="false" apiLink="https://thewedders.in/xmlrpc.php" />
-			<api name="Blogger" blogID="1" preferred="false" apiLink="https://thewedders.in/xmlrpc.php" />
-				<api name="WP-API" blogID="1" preferred="false" apiLink="https://thewedders.in/wp-json/" />
+			<api name="WordPress" blogID="1" preferred="true" apiLink="https://rawleavesphotography.in/xmlrpc.php" />
+			<api name="Movable Type" blogID="1" preferred="false" apiLink="https://rawleavesphotography.in/xmlrpc.php" />
+			<api name="MetaWeblog" blogID="1" preferred="false" apiLink="https://rawleavesphotography.in/xmlrpc.php" />
+			<api name="Blogger" blogID="1" preferred="false" apiLink="https://rawleavesphotography.in/xmlrpc.php" />
+				<api name="WP-API" blogID="1" preferred="false" apiLink="https://rawleavesphotography.in/wp-json/" />
 			</apis>
 	</service>
 </rsd>
